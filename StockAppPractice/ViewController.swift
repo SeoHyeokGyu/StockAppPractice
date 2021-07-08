c379b096c6a83868881a272bc75741864f05084a
SeoHyeokGyu <shk64099@gmail.com>
2021-07-08 14:09:54 +0900

    Initial Commit

---
+++ StockAppPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockAppPractice
+//
+//  Created by 서혁규 on 2021/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
