655078bdb5643dbe2ad0c80a7d7147d3edd5fce4
SeoHyeokGyu <shk64099@gmail.com>
2021-07-14 21:23:59 +0900

    #새로운 모델, 호출할 수 있는 API를 호출할수 있는 함수 생성, 과정 중 발생할 수있는 에러케이스를 막는코드 작성, 인코딩, 커스텀 에러 작성, 코드스타일링 개선

---
+++ StockAppPractice/Model/TimeSeriesMonthlyAdjusted.swift
@@ -0,0 +1,40 @@
+//
+//  TimeSeriesMonthlyAdjusted.swift
+//  StockAppPractice
+//
+//  Created by 서혁규 on 2021/07/14.
+//
+
+import Foundation
+
+struct TimeSeriesMonthlyAdjusted: Decodable{
+    
+    let meta: Meta
+    let series: [String: OHLC]
+    
+    enum CodingKeys: String, CodingKey {
+        case meta = "Meta Data"
+        case series = "Monthly Adjusted Time Series"
+    }
+    
+    struct OHLC: Decodable {
+        let open: String
+        let close: String
+        let adjustedClose: String
+        
+        enum CodingKeys: String, CodingKey {
+            case open = "1. open"
+            case close = "4. close"
+            case adjustedClose = "5. adjusted close"
+        }
+    }
+    
+    
+    struct Meta: Decodable {
+        let symbol: String
+        
+        enum CodingKeys: String, CodingKey {
+            case symbol = "2. Symbol"
+        }
+    }
+}
--- StockAppPractice/Repository/StockRepositoryImpl.swift
+++ StockAppPractice/Repository/StockRepositoryImpl.swift
@@ -18,19 +18,64 @@ class SrockRepositoryImpl:StockRepository{
     var apiKey: String = "6VWET1UP1DJVXFCH"
     
     func fetchStockPublisher(keywords: String) -> AnyPublisher<StockResult, Error>{
-        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
-            let error: StockRepositoryError = .encoding
-            return Fail(error: error).eraseToAnyPublisher()
-        }
-        let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)"
         
-        guard let url = URL(string: urlString) else {
-            let error: StockRepositoryError = .bad_url
+        let queryResult = parseQueryString(text: keywords)
+        var query = ""
+        switch queryResult {
+        case .failure(let error):
             return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
         }
         
-        return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+        let urlResult = parseURL(urlString: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)")
+        
+        switch urlResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+        }
         
     }
-
+    
+    func fetchTimeSeriesSeriesPublisher(keywords: String) -> AnyPublisher<TimeSeriesMonthlyAdjusted, Error>{
+        
+        let queryResult = parseQueryString(text: keywords)
+        var query = ""
+        switch queryResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
+        }
+        
+        let urlResult = parseURL(urlString:  "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(keywords)&apikey=\(apiKey)")
+        
+        switch urlResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: TimeSeriesMonthlyAdjusted.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+        }
+        
+    }
+    private func parseURL(urlString: String) -> Result<URL, Error>{
+        if let url = URL(string: urlString) {
+            return .success(url)
+        } else {
+            let error: StockRepositoryError = .bad_url
+            return .failure(error)
+        }
+    }
+    
+    private func parseQueryString(text: String) -> Result<String, Error> {
+        if let query = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed){
+            return .success(query)
+        } else{
+            let error: StockRepositoryError = .encoding
+            return .failure(error)
+        }
+    }
+    
 }
