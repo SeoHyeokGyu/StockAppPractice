2aa79e0eacd9ab51a9531bb4fab984235138d6d2
SeoHyeokGyu <shk64099@gmail.com>
2021-07-14 20:58:09 +0900

    #Fix 검색할때 띄어쓰기 처리

--- StockAppPractice/Repository/StockRepositoryImpl.swift
+++ StockAppPractice/Repository/StockRepositoryImpl.swift
@@ -9,14 +9,23 @@ import Combine
 
 class SrockRepositoryImpl:StockRepository{
     
+    enum StockRepositoryError: Error {
+        case encoding
+        case bad_url
+    }
+    
     let decoder = JSONDecoder()
     var apiKey: String = "6VWET1UP1DJVXFCH"
     
     func fetchStockPublisher(keywords: String) -> AnyPublisher<StockResult, Error>{
+        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            let error: StockRepositoryError = .encoding
+            return Fail(error: error).eraseToAnyPublisher()
+        }
         let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)"
         
         guard let url = URL(string: urlString) else {
-            let error = URLError(.badURL)
+            let error: StockRepositoryError = .bad_url
             return Fail(error: error).eraseToAnyPublisher()
         }
         
